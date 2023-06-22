d46fcd6fb8015ee0aed84dc26f97642de740c32a
AGAMEN0M <106293929+AGAMEN0M@users.noreply.github.com>
2023-06-22 20:13:15 -0300

    Delete Assets directory

--- Assets/Language/Script/Legacy/LanguageFontList.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-public class LanguageFontList : MonoBehaviour
-{
-    public List<Font> FontList; //List of fonts for the LanguageText script.
-}
--- Assets/Language/Script/TextMesh Pro/LanguageFontListTextMeshPro.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-using TMPro;
-
-public class LanguageFontListTextMeshPro : MonoBehaviour
-{
-    public List<TMP_FontAsset> FontListTextMeshPro; //List of fonts for the LanguageText script.
-}
